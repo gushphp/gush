9a1a613c940070ad20d686efc0248dc1bf003c9c
Luis Cordova <cordoval@gmail.com>
2014-01-09 12:02:18 -0500

    bootstrap refactor of test

--- tests/Gush/Tests/Command/PullRequestMergeCommandTest.php
+++ tests/Gush/Tests/Command/PullRequestMergeCommandTest.php
@@ -20,6 +20,31 @@ class PullRequestMergeCommandTest extends BaseTestCase
 {
     public function testCommand()
     {
+        $this->expectShowPullRequest();
+        $this->expectPullRequestCommits();
+        $this->expectPullRequestMerge();
+
+        $tester = $this->getCommandTester(new PullRequestMergeCommand());
+        $tester->execute(['org' => 'cordoval', 'prNumber' => 40]);
+
+        $this->assertEquals("Pull Request successfully merged.", trim($tester->getDisplay()));
+    }
+
+    protected function expectShowPullRequest()
+    {
+        $this->httpClient->whenGet(
+            'repos/cordoval/gush/pulls/40/merge',
+            json_encode(['commit_message' => 'Merged using Gush'])
+        )->thenReturn(
+            [
+                'merged' => true,
+                'message' => 'Pull Request successfully merged.',
+            ]
+        );
+    }
+
+    protected function expectPullRequestCommits()
+    {
         $this->httpClient->whenPut(
             'repos/cordoval/gush/pulls/40/merge',
             json_encode(['commit_message' => 'Merged using Gush'])
@@ -29,10 +54,18 @@ class PullRequestMergeCommandTest extends BaseTestCase
                 'message' => 'Pull Request successfully merged.',
             ]
         );
+    }
 
-        $tester = $this->getCommandTester(new PullRequestMergeCommand());
-        $tester->execute(['org' => 'cordoval', 'prNumber' => 40]);
-
-        $this->assertEquals("Pull Request successfully merged.", trim($tester->getDisplay()));
+    protected function expectPullRequestMerge()
+    {
+        $this->httpClient->whenPut(
+            'repos/cordoval/gush/pulls/40/merge',
+            json_encode(['commit_message' => 'Merged using Gush'])
+        )->thenReturn(
+            [
+                'merged' => true,
+                'message' => 'Pull Request successfully merged.',
+            ]
+        );
     }
 }
