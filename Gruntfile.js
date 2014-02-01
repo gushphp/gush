fa40a95c4e6f9161fbe6cb6808970cbcb85fa4aa
Luis Cordova <cordoval@gmail.com>
2014-02-01 11:55:56 -0500

    plug missing headers and clean up .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,12 @@
+/**
+ * This file is part of Gush.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 module.exports = function (grunt) {
     grunt.initConfig({
         shell: {
