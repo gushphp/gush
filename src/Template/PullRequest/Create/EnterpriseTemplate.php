ab8c9fe2bc4872eb4dcaddf3e66f7ce44b5584b6
Luis Cordova <cordoval@gmail.com>
2014-03-20 18:45:07 -0700

    plug new template for enterprise

---
+++ src/Template/PullRequest/Create/EnterpriseTemplate.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of Gush.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Gush\Template\PullRequest\Create;
+
+/**
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class EnterpriseTemplate extends AbstractSymfonyTemplate
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getRequirements()
+    {
+        return [
+            'bug_fix' => ['Bug Fix?', 'no'],
+            'new_feature' => ['New Feature?', 'no'],
+            'bc_breaks' => ['BC Breaks?', 'no'],
+            'deprecations' => ['Deprecations?', 'no'],
+            'tests_pass' => ['Tests Pass?', 'yes'],
+            'fixed_tickets' => ['Fixed Tickets', ''],
+            'license' => ['License', 'Proprietary'],
+            'doc_pr' => ['Doc PR', ''],
+            'description' => ['Description', ''],
+        ];
+    }
+
+    public function getName()
+    {
+        return 'pull-request-create/enterprise';
+    }
+}
