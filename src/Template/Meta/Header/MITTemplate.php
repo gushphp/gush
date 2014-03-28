ee03c70ea234cf92a6280ca244d4f1c61a943089
Luis Cordova <cordoval@gmail.com>
2014-03-28 12:04:51 -0500

    addressing some more comments

--- src/Template/Meta/Header/MITTemplate.php
+++ src/Template/Meta/Header/MITTemplate.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Gush.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Gush\Template\Meta\Header;
 
 use Gush\Template\AbstractTemplate;
@@ -31,9 +40,7 @@ EOT;
 
     public function render()
     {
-        $params = array_merge(array(
-            'copyright-to' => date('Y')
-        ), $this->parameters);
+        $params = array_merge(['copyright-to' => date('Y')], $this->parameters);
 
         return $this->replaceTokens($this->header, $params);
     }
