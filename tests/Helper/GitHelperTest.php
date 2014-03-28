8833576e0905255874116fd48d7ef84fdc9e0c0c
Luis Cordova <cordoval@gmail.com>
2014-03-28 11:18:00 -0500

    introduce slight changes to minimize diffs

--- tests/Helper/GitHelperTest.php
+++ tests/Helper/GitHelperTest.php
@@ -28,7 +28,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itBearsTheRightHelperName()
+    public function it_bears_the_right_helper_name()
     {
         $this->assertEquals('git', $this->git->getName());
     }
@@ -36,7 +36,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itGetsCurrentGitBranchName()
+    public function it_gets_current_git_branch_name()
     {
         exec('git rev-parse --abbrev-ref HEAD', $output);
         $this->assertEquals($output[0], $this->git->getBranchName());
@@ -45,7 +45,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itGetsTheLastTagOnTheCurrentBranch()
+    public function it_gets_the_last_tag_on_the_current_branch()
     {
         exec('git describe --tags --abbrev=0 HEAD', $output);
         $this->assertEquals($output[0], $this->git->getLastTagOnCurrentBranch());
@@ -54,7 +54,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itGetsTheRepositoryName()
+    public function it_gets_the_repository_name()
     {
         $this->assertEquals('gush', $this->git->getRepoName());
     }
@@ -62,7 +62,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itGetsTheVendorNameOfTheRepository()
+    public function it_gets_the_vendor_name_of_the_repository()
     {
         $this->assertEquals(getenv('GIT_VENDOR_NAME'), $this->git->getVendorName());
     }
@@ -70,7 +70,7 @@ class GitHelperTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itRunsGitCommand()
+    public function it_runs_git_command()
     {
         $this->markTestIncomplete('needs to be written');
     }
