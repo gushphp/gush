6ba94c6c165cae831d1ab41fd79ece052bf0b462
Luis Cordova <cordoval@gmail.com>
2014-04-20 23:02:37 -0500

    run meta:header command thanks to pierre du plesis

--- src/Command/PullRequestSwitchBaseCommand.php
+++ src/Command/PullRequestSwitchBaseCommand.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Gush.
+ * This file is part of Gush package.
  *
- * (c) Luis Cordova <cordoval@gmail.com>
+ * (c) 2013-2014 Luis Cordova <cordoval@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
