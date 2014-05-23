c00db6ce349eb9de945b347402c605aca6da7d28
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-05-23 11:20:07 +0200

    feat: add Configurator interface
    This decouples the Adapter configuration from the Adapter
    and 'core:configure' command, allows for flexible configuration
    and feature enhancements

---
+++ src/Adapter/Configurator.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of Gush package.
+ *
+ * (c) 2013-2014 Luis Cordova <cordoval@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Gush\Adapter;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Configurator is the interface implemented by all Gush Adapter Configurator classes.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+interface Configurator
+{
+    const AUTH_HTTP_PASSWORD = 'http_password';
+
+    const AUTH_HTTP_TOKEN = 'http_token';
+
+    /**
+     * Configures the adapter for usage.
+     *
+     * This methods is called for building the adapter configuration
+     * which will be used every time a command is executed with adapter.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return array Validated and normalized configuration as associative array
+     *
+     * @throws \Exception When any of the validators returns an error
+     */
+    public function interact(InputInterface $input, OutputInterface $output);
+}
