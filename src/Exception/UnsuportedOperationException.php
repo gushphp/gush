41741deec34d2f31fb8a501635200842c24d5898
jubianchi <contact@jubianchi.fr>
2014-06-05 20:29:49 +0200

    Add an exception for adapters' unsupported operations

---
+++ src/Exception/UnsuportedOperationException.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Gush.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Gush\Exception;
+
+/**
+ * Exception thrown by adapters when they do not support an operation
+ *
+ * @author Julien Bianchi <contact@jubianchi.fr>
+ */
+class UnsuportedOperationException extends AdapterException
+{
+}
