1bc9a78727b521cfc26828553663d1fc485335b4
Luciano Mammino <lmammino@oryzone.com>
2014-07-31 12:44:45 +0200

    Add example load of providers into the container

--- src/Container.php
+++ src/Container.php
@@ -2,9 +2,18 @@
 
 namespace Gush;
 
+use Gush\Services\HelpersProvider;
+use Gush\Services\MetaProvider;
 use Pimple\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
-
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct()
+    {
+        $this->register(new HelpersProvider());
+        $this->register(new MetaProvider());
+    }
 }
