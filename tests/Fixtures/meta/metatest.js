d0b159d8d084324ee5615112a69d585529716103
Luis Cordova <cordoval@gmail.com>
2014-10-17 06:18:30 -0500

    revert changes on fixtures

--- tests/Fixtures/meta/metatest.js
+++ tests/Fixtures/meta/metatest.js
@@ -1,12 +1,3 @@
-/*
- * This file is part of Gush package.
- *
- * (c) 2013-2014 Luis Cordova <cordoval@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 (function ($) {
     $.fn.someFunction = function () {
         return $(this).append('New Function');
