a9e63e7950b309e14d9a432f41a437d556044411
Luis Cordova <cordoval@gmail.com>
2015-01-10 19:35:43 -0500

    update with new info from .gush.yml updated to 2015

--- src/Event/GushEvents.php
+++ src/Event/GushEvents.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Gush package.
  *
- * (c) 2013-2014 Luis Cordova <cordoval@gmail.com>
+ * (c) 2013-2015 Luis Cordova <cordoval@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
