21d62f709158a7110efe538f598d3a7c5da8406c
Luis Cordova <cordoval@gmail.com>
2015-04-15 04:59:44 -0500

    remove year in stub and updated license year

--- phar-stub.php
+++ phar-stub.php
@@ -4,7 +4,7 @@
 /*
  * This file is part of Gush package.
  *
- * (c) 2013-2015 Luis Cordova <cordoval@gmail.com>
+ * (c) Luis Cordova <cordoval@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
