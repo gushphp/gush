af9f2f895f9d8bc3aef81bfe5b9b33ca7d0ecfcf
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-05-03 11:06:49 +0200

    fix PullRequestSquashCommand

--- src/Command/PullRequest/PullRequestSquashCommand.php
+++ src/Command/PullRequest/PullRequestSquashCommand.php
@@ -12,11 +12,13 @@
 namespace Gush\Command\PullRequest;
 
 use Gush\Command\BaseCommand;
+use Gush\Exception\UserException;
 use Gush\Feature\GitRepoFeature;
 use Gush\Helper\GitConfigHelper;
 use Gush\Helper\GitHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PullRequestSquashCommand extends BaseCommand implements GitRepoFeature
@@ -29,13 +31,25 @@ class PullRequestSquashCommand extends BaseCommand implements GitRepoFeature
         $this
             ->setName('pull-request:squash')
             ->setDescription('Squashes all commits of a pull request')
+            ->addOption(
+                'no-local-sync',
+                null,
+                InputOption::VALUE_NONE,
+                'Do not sync the local branch with the squashed version'
+            )
             ->addArgument('pr_number', InputArgument::REQUIRED, 'pull-request number to squash')
             ->setHelp(
                 <<<EOF
-The <info>%command.name%</info> command squashes all commits of a PR:
+The <info>%command.name%</info> command squashes all commits of a pull-request:
 
     <info>$ gush %command.name% 12</info>
 
+Make sure you are the pull-requests\'s source-branch repository owner
+or have been granted push access to the repository.
+
+Note: This will squash all commits in the pull-request and (when a local branch exists with
+the same name) sync your local source branch with the squashed version.
+You can skip this sync-process using the <comment>--no-local-sync</> option.
 EOF
             )
         ;
@@ -51,29 +65,68 @@ EOF
         $adapter = $this->getAdapter();
         $pr = $adapter->getPullRequest($prNumber);
 
-        $base = $pr['base']['ref'];
-        $head = $pr['head']['ref'];
+        $this->checkOwner($input, $pr);
+
+        $baseOrg = $pr['base']['user'];
+        $baseBranch = $pr['base']['ref'];
 
         $sourceOrg = $pr['head']['user'];
-        $branchName = $pr['head']['ref'];
+        $sourceBranch = $pr['head']['ref'];
 
         /** @var GitConfigHelper $gitConfigHelper */
         $gitConfigHelper = $this->getHelper('git_config');
-        $gitConfigHelper->ensureRemoteExists($pr['user'], $pr['head']['repo']);
+        $gitConfigHelper->ensureRemoteExists($baseOrg, $pr['base']['repo']);
+        $gitConfigHelper->ensureRemoteExists($sourceOrg, $pr['head']['repo']);
 
         /** @var GitHelper $gitHelper */
         $gitHelper = $this->getHelper('git');
+        $gitHelper->remoteUpdate($baseOrg);
         $gitHelper->remoteUpdate($sourceOrg);
 
-        /** @var GitHelper $gitHelper */
-        $gitHelper = $this->getHelper('git');
-        $gitHelper->squashCommits($base, $head);
-        $gitHelper->pushToRemote('origin', $head, false, true);
+        $gitHelper->stashBranchName();
+        $gitHelper->checkout($sourceBranch);
+        $gitHelper->checkout($tmpBranch = $gitHelper->createTempBranch($sourceBranch), true);
+        $gitHelper->squashCommits($baseOrg.'/'.$baseBranch, $tmpBranch);
+        $gitHelper->pushToRemote($sourceOrg, $tmpBranch.':'.$sourceBranch, false, true);
+
+        if (!$input->getOption('no-local-sync') && $gitHelper->branchExists($sourceBranch)) {
+            $gitHelper->checkout($sourceBranch);
+            $gitHelper->reset($tmpBranch, 'hard');
+        }
+
+        $gitHelper->restoreStashedBranch();
 
         $adapter->createComment($prNumber, '(PR squashed)');
 
         $this->getHelper('gush_style')->success('Pull-request has been squashed!');
 
         return self::COMMAND_SUCCESS;
+    }
+
+    private function checkOwner(InputInterface $input, array $pr)
+    {
+        // Don't check when non-interactive or when pull-request is on target org already
+        if (!$input->isInteractive() || $pr['head']['user'] === $input->getOption('org')) {
+            return;
+        }
+
+        if ($pr['head']['user'] === $this->getParameter($input, 'authentication')['username']) {
+            return;
+        }
+
+        $this->getHelper('gush_style')->note(
+            [
+                'You are not the owner of the repository pull-requests\'s source-branch.',
+                sprintf(
+                    'Make sure you have push access to the "%s/%s" repository before you continue.',
+                    $pr['head']['user'],
+                    $pr['head']['repo']
+                )
+            ]
+        );
+
+        if (!$this->getHelper('gush_style')->confirm('Do you want to squash the pull-request and push?')) {
+            throw new UserException('User aborted.');
+        }
     }
 }
--- tests/Command/PullRequest/PullRequestSquashCommandTest.php
+++ tests/Command/PullRequest/PullRequestSquashCommandTest.php
@@ -13,6 +13,7 @@ namespace Gush\Tests\Command\PullRequest;
 
 use Gush\Command\PullRequest\PullRequestSquashCommand;
 use Gush\Tests\Command\CommandTestCase;
+use Symfony\Component\Console\Helper\HelperSet;
 
 class PullRequestSquashCommandTest extends CommandTestCase
 {
@@ -24,12 +25,140 @@ class PullRequestSquashCommandTest extends CommandTestCase
         $this->assertCommandOutputMatches('Pull-request has been squashed!', $tester->getDisplay());
     }
 
-    protected function getGitHelper($isGitFolder = true)
+    public function testDoNotResetLocalBranch()
     {
-        $gitHelper = parent::getGitHelper($isGitFolder);
-        $gitHelper->squashCommits('base_ref', 'head_ref')->shouldBeCalled();
-        $gitHelper->pushToRemote('origin', 'head_ref', false, true)->shouldBeCalled();
+        $command = new PullRequestSquashCommand();
+        $tester = $this->getCommandTester(
+            $command,
+            null,
+            null,
+            function (HelperSet $helperSet) {
+                $helperSet->set($this->getGitHelper(true, true, false)->reveal());
+            }
+        );
 
-        return $gitHelper;
+        $tester->execute(['pr_number' => 10, '--no-local-sync' => true]);
+
+        $this->assertCommandOutputMatches('Pull-request has been squashed!', $tester->getDisplay());
+    }
+
+    public function testDoNotResetLocalBranchWhenIsMissing()
+    {
+        $command = new PullRequestSquashCommand();
+        $tester = $this->getCommandTester(
+            $command,
+            null,
+            null,
+            function (HelperSet $helperSet) {
+                $helperSet->set($this->getGitHelper(true, false, false)->reveal());
+            }
+        );
+
+        $tester->execute(['pr_number' => 10]);
+
+        $this->assertCommandOutputMatches('Pull-request has been squashed!', $tester->getDisplay());
+    }
+
+    public function testWarnPossibleAccessDenied()
+    {
+        $command = new PullRequestSquashCommand();
+        $tester = $this->getCommandTester(
+            $command,
+            [
+                'adapters' => [
+                    'github_enterprise' => [
+                        'authentication' => [
+                            'username' => 'someone',
+                            'password' => 'very-un-secret',
+                        ]
+                    ]
+                ]
+            ],
+            null,
+            function (HelperSet $helperSet) {
+                $helperSet->set($this->getGitHelper(true, false, false)->reveal());
+            }
+        );
+
+        $this->setExpectedCommandInput($command, "yes\n");
+
+        $tester->execute(['pr_number' => 10]);
+
+        $this->assertCommandOutputMatches(
+            [
+                'Pull-request has been squashed!',
+                'You are not the owner of the repository pull-requests\'s source-branch.',
+                'Make sure you have push access to the "cordoval/gush" repository before you continue.',
+                'Do you want to squash the pull-request and push?'
+            ],
+            $tester->getDisplay()
+        );
+    }
+
+    public function testDoesNotWarnPossibleAccessDeniedWhenBranchIsOnSameOrg()
+    {
+        $command = new PullRequestSquashCommand();
+        $tester = $this->getCommandTester(
+            $command,
+            [
+                'adapters' => [
+                    'github_enterprise' => [
+                        'authentication' => [
+                            'username' => 'someone',
+                            'password' => 'very-un-secret',
+                        ]
+                    ]
+                ]
+            ],
+            null,
+            function (HelperSet $helperSet) {
+                $helperSet->set($this->getGitHelper(true, false, false)->reveal());
+            }
+        );
+
+        // Prevent hanging when the test fails.
+        $this->setExpectedCommandInput($command, "no\n");
+
+        $tester->execute(['pr_number' => 10, '--org' => 'cordoval']);
+
+        $display = $tester->getDisplay();
+
+        $this->assertCommandOutputMatches('Pull-request has been squashed!', $display);
+        $this->assertNotContains('You are not the owner of the repository.', $display);
+    }
+
+    protected function getGitConfigHelper()
+    {
+        $helper = parent::getGitConfigHelper();
+        $helper->ensureRemoteExists('gushphp', 'gush')->shouldBeCalled();
+        $helper->ensureRemoteExists('cordoval', 'gush')->shouldBeCalled();
+
+        return $helper;
+    }
+
+    protected function getGitHelper($isGitFolder = true, $branchExists = true, $localSync = true)
+    {
+        $helper = parent::getGitHelper($isGitFolder);
+        $helper->remoteUpdate('gushphp')->shouldBeCalled();
+        $helper->remoteUpdate('cordoval')->shouldBeCalled();
+
+        $helper->stashBranchName()->shouldBeCalled();
+        $helper->checkout('head_ref')->shouldBeCalled();
+
+        $helper->createTempBranch('head_ref')->willReturn('temp--head_ref');
+        $helper->checkout('temp--head_ref', true)->shouldBeCalled();
+
+        $helper->squashCommits('gushphp/base_ref', 'temp--head_ref')->shouldBeCalled();
+        $helper->pushToRemote('cordoval', 'temp--head_ref:head_ref', false, true)->shouldBeCalled();
+
+        $helper->branchExists('head_ref')->willReturn($branchExists);
+
+        if ($localSync) {
+            $helper->reset('temp--head_ref', 'hard')->shouldBeCalled();
+        }
+
+        $helper->restoreStashedBranch()->shouldBeCalled();
+
+        return $helper;
     }
 }
