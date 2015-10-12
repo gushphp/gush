131cfd9ed89439b82d06f168f7bc25ce71800826
Luis Cordova <cordoval@gmail.com>
2015-10-11 20:20:38 -0500

    remove headers duplicated

--- src/ThirdParty/Jira/JiraEnterpriseIssueTracker.php
+++ src/ThirdParty/Jira/JiraEnterpriseIssueTracker.php
@@ -9,15 +9,6 @@
  * with this source code in the file LICENSE.
  */
 
-/**
- * This file is part of Gush.
- *
- * (c) Luis Cordova <cordoval@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace Gush\ThirdParty\Jira;
 
 /**
