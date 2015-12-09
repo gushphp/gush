40032b6bbfa72f877dfe6c33bfd655591a3f6574
Stéphane HULARD <s.hulard@chstudio.fr>
2015-12-09 11:08:36 +0100

    Gitlab: Allow to list issue tracker labels

--- src/ThirdParty/Gitlab/Adapter/GitLabIssueTracker.php
+++ src/ThirdParty/Gitlab/Adapter/GitLabIssueTracker.php
@@ -12,8 +12,8 @@
 namespace Gush\ThirdParty\Gitlab\Adapter;
 
 use Gush\Adapter\BaseIssueTracker;
-use Gush\Exception\UnsupportedOperationException;
 use Gush\ThirdParty\Gitlab\Model\Issue;
+use Gush\Util\ArrayUtil;
 
 /**
  * @author Luis Cordova <cordoval@gmail.com>
@@ -179,7 +179,10 @@ class GitLabIssueTracker extends BaseIssueTracker
      */
     public function getLabels()
     {
-        throw new UnsupportedOperationException('Labels are not supported by Gitlab');
+        return ArrayUtil::getValuesFromNestedArray(
+            $this->client->api('projects')->labels($this->getCurrentProject()->id),
+            'name'
+        );
     }
 
     /**
