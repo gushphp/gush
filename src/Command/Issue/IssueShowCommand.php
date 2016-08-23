cedc950cd79c466f4a2656608ccd14d13f30c110
Stéphane HULARD <s.hulard@chstudio.fr>
2016-08-23 23:40:18 +0200

    issue:show: Simplify logic / update display method

--- src/Command/Issue/IssueShowCommand.php
+++ src/Command/Issue/IssueShowCommand.php
@@ -60,9 +60,6 @@ EOF
         $comments = [];
         $tracker = $this->getIssueTracker();
         $issue = $tracker->getIssue($issueNumber);
-        if (true === $input->getOption('with-comments')) {
-            $comments = $tracker->getComments($issueNumber);
-        }
 
         $styleHelper = $this->getHelper('gush_style');
         $styleHelper->title(
@@ -89,20 +86,21 @@ EOF
         $styleHelper->section('Body');
         $styleHelper->text(explode("\n", $issue['body']));
 
-        if (true === $input->getOption('with-comments') && count($comments) > 0) {
+        if (true === $input->getOption('with-comments')) {
+            $comments = $tracker->getComments($issueNumber);
             $styleHelper->section('Comments');
             foreach ($comments as $comment) {
-                $styleHelper->listing([
-                    sprintf(
-                        'Comment #%s by %s on %s',
-                        $comment['id'],
-                        $comment['user'],
-                        $comment['created_at']->format('r')
-                    ),
-                    'Link: '.$comment['url'],
-                    '',
-                    wordwrap($comment['body'], 100),
+                $styleHelper->text(sprintf(
+                    '<fg=white>Comment #%s</> by %s on %s',
+                    $comment['id'],
+                    $comment['user'],
+                    empty($comment['created_at'])?'':$comment['created_at']->format('r')
+                ));
+                $styleHelper->detailsTable([
+                    ['Link', $comment['url']],
                 ]);
+                $styleHelper->text(explode("\n", wordwrap($comment['body'], 100)));
+                $styleHelper->text([]);
             }
         }
 
